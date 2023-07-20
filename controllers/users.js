456fdd24d728347a36e4c130944ba7d7a0524abe
MarisaCodes <marisakirisame955@gmail.com>
2023-07-20 19:33:19 +0400

    altered sign up page layout finalized blog cards, doing server-side logic on sign up

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const crypto = require("crypto");
 const sql = require("../models/db");
 
 // get signup page
@@ -6,10 +7,19 @@ const get_sign_up = (req, res) => {
 };
 
 // post signup info
-
+const is_user_unique = (username) => {
+  sql`select user_name from public.users where user_name = ${username}`
+  .then()
+};
 const post_sign_up = (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
+  const { username, pswd } = req.body;
+  const hash = crypto.createHash("sha256");
+  const pswd_hash = hash.update(pswd).digest("hex");
+  sql`insert into public.users (user_name, password_hash) values (${username}, ${pswd_hash})`
+    .then((rep) => {
+      res.json(rep);
+    })
+    .catch(console.log);
 };
 
 module.exports = { get_sign_up, post_sign_up };
--- models/db.js
+++ models/db.js
@@ -11,6 +11,5 @@ const url_data = {
 const sql = postgres(
   `postgres://${url_data.username}:${url_data.pswd}@localhost:${url_data.port}/${url_data.db_name}`
 );
-
-
-//module.exports = sql;
+sql`select user_name from users where user_name = 'reimu'`.then(console.log)
+module.exports = sql;
